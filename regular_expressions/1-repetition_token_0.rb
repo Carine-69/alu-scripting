43759e6b2a87a0815230129dcf13c3a868a22a7d
Carine-69 <c.umugabeka@alustudent.com>
2024-05-10 02:56:24 -0700

    task 1

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,15 @@
 #!/usr/bin/env ruby
+
+# Check if the argument is provided
 abort "Usage: #{$PROGRAM_NAME} <string>" if ARGV.empty?
-puts ARGV[0].scan(/hbt{2,5}n/).join
+
+# Get the argument from the command line
+string = ARGV[0]
+
+# Define the regular expression pattern and check for matches
+match = string.match(/hbt{2,5}n/)
+
+# If there's a match, print the matched text
+puts match[0] if match
+
 

ea0520a1af90e12ad6df6b318d0b18f449e8a55c
Carine-69 <c.umugabeka@alustudent.com>
2024-05-11 10:15:01 -0700

    task 1

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,15 +1,3 @@
 #!/usr/bin/env ruby
-
-# Check if the argument is provided
-abort "Usage: #{$PROGRAM_NAME} <string>" if ARGV.empty?
-
-# Get the argument from the command line
-string = ARGV[0]
-
-# Define the regular expression pattern and check for matches
-match = string.match(/hbt{2,5}n/)
-
-# If there's a match, print the matched text
-puts match[0] if match
-
+puts ARGV[0].scan(/hbt{2,5}n/).join
 

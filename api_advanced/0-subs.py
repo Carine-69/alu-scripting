98cfe468997ec3026a1a4dc01a3e99fc1cd88385
Carine-69 <umugabecarine@gmail.com>
2024-07-04 14:16:00 +0000

    returning all subscribers

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Combined script to get the number of subscribers for a given subreddit
+"""
+
+import sys
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
